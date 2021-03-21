282ca08c38954c24d3fa82ea4bc9a9ef8fe239e5
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-03-21 09:25:37 +0530

    views, text and Styles

--- App.js
+++ App.js
@@ -5,7 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Lorem ipsumdolar cimets!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +20,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20
+  }
 });
