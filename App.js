a25ccb46ea1849b24a13a5238a9b7c29a0c3a5ec
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-03-22 16:14:26 +0530

    list and scroll

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState("Rishabh Mishra");
-  const [age, setAge] = useState(20);
+  const [people, setPeople] = useState([
+    {name: "Rishabh", key: '1'},
+    {name: "Digvijay", key: "2"},
+    {name: "Arnav", key:"3"},
+    {name: "Ekagra", key:"4"},
+    {name: "Rishabh", key: '5'},
+    {name: "Digvijay", key: "6"},
+    {name: "Arnav", key:"7"},
+    {name: "Ekagra", key:"8"}
+  ])
 
   return (
     <View style={styles.container}>
-      <Text>Enter Name: </Text>
-      <TextInput 
-        multiline
-        style={styles.input} 
-        placeholder='Ex: Rishabh Mishra'
-        onChangeText={(value) => setName(value)}/>
-
-      <Text>Enter Age: </Text>
-      <TextInput 
-        keyboardType='numeric'
-        style={styles.input} 
-        placeholder='Ex: 20'
-        onChangeText={(value) => setAge(value)}/>
-
-      <Text>My Name is {name} and age is {age}</Text>
+      <ScrollView>
+        {people.map((x) => {
+        return (
+          <View key={x.key}>
+            <Text style={styles.item}>{x.name}</Text>
+          </View>
+        )
+      })}
+      </ScrollView>
       
       <StatusBar style="auto" />
     </View>
@@ -33,14 +35,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#2ff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200
+  item:{
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24
   }
+  
 });
