461d0870e2ebc9a2d443abd2cb01f6b7443708b5
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-03-23 07:26:55 +0530

    ToDo Items completed

--- App.js
+++ App.js
@@ -2,16 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import Header from './components/header'
 import { FlatList, StyleSheet, Text, View } from 'react-native';
+import ToDoItems from './components/todoitems';
 
 export default function App() {
     const [todos, setTodos] = useState([
       {text: "Buy A Coffee", key:"1"},
       {text: "Create an app", key: "2"}
-    ])
+    ]);
+
+    const pressHandler = (key) => {
+      setTodos((prevToDos) => {
+        return prevToDos.filter((todo) => todo.key != key )
+      })
+    }
   
   return (
     <View style={styles.container}>
-      {/* header */}
         <Header />
     <View style={styles.content}>
       {/* to form */}
@@ -21,7 +27,7 @@ export default function App() {
         <FlatList
           data={todos}
           renderItem={({ item }) => (
-            <Text>{item.text}</Text>
+            <ToDoItems item={item} pressHandler={pressHandler} />
           )}
         />
       </View>
